5973f9e6358ca2162151a7a9b824750141d3e76b
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-12-18 16:34:03 +0100

    Added registration feature

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $userRepository = $this->getDoctrine()->getManager()->getRepository(User::class);
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid() && empty($userRepository->findBy(['username' => $user->getUsername()])) && empty($userRepository->findBy(['email' => $user->getEmail()]))) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_registration');
+        }
+
+        return $this->render('registration/register.html.twig', array('form' => $form->createView())
+        );
+    }
+}
