0e4992d898e570a1161cf909fa54740aff8853e6
Maxime Maréchal <maxime.marechal@supinternet.fr>
2019-01-02 16:05:57 +0100

    Changed the rediretion after login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-         return new RedirectResponse($this->router->generate('login'));
+         return new RedirectResponse($this->router->generate('main'));
 //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
